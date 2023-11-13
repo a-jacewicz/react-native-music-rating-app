dcbbc45e1db9fce78b503eecf0758b7ffa50d054
khentrekin <khentrekin5578@gmail.com>
2023-11-13 17:14:38 -0500

    Adding routes

--- music-app/App.js
+++ music-app/App.js
@@ -1,19 +1,33 @@
 import { StyleSheet, Text, View } from "react-native";
 import Read from "./Components/Read";
 import Register from "./Components/Register";
-import Logout from "./Components/LogOut";
+import Individual from "./Components/Individual";
+// import Logout from "./Components/LogOut";
 import Create from "./Components/Create";
 import Login from "./Components/Login";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Read />
-      <Register />
-      <Logout />
-    <Create />
-      <Login />
-    </View>
+    // <View style={styles.container}>
+    //   <Read />
+    //   <Register />
+    //   <Logout />
+    //   <Create />
+    //   <Login />
+    // </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Read">
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Read" component={Read} />
+        <Stack.Screen name="Create" component={Create} />
+        <Stack.Screen name="Individual" component={Individual} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
