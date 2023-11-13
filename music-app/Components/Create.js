d43ea5c513c2285377bd15593ad2f23947636088
ajacewicz <ajacewicz@wesleyan.edu>
2023-11-13 13:26:37 -0500

    add fetch function

--- music-app/Components/Create.js
+++ music-app/Components/Create.js
@@ -8,3 +8,30 @@ import {
   StyleSheet,
 } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+export default function Create() {
+    const [title, setTitle] = useState("");
+    const [artist, setArtist] = useState("");
+    const [rating, setRating] = useState("");
+
+    const handleCreate = async () => {
+        try {
+          const response = await fetch(
+           // kelleighs IP
+           // "http://172.21.250.15:8081/index.php/user/register",
+           // aleks IP address 
+           'http://172.21.98.195/index.php/rating/create', 
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                artist: artist,
+                song: song,
+                rating: rating,
+              }),
+            }
+          );
