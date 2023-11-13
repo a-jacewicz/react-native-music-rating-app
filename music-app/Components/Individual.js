8a7e2316cef0b2434467039341392e3777dda571
khentrekin <khentrekin5578@gmail.com>
2023-11-13 12:16:20 -0500

    Added data constants

--- music-app/Components/Individual.js
+++ music-app/Components/Individual.js
@@ -1,20 +1,29 @@
 import { StyleSheet, Text, View, Image, Button } from "react-native";
 import { Rating } from "react-native-ratings";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export default function Individual() {
+export default function Individual({ route }) {
+  const { data } = route.params;
+
+  const [username, setUsername] = useState(data.username);
+  const [artist, setArtist] = useState(data.artist);
+  const [song, setSong] = useState(data.song);
+  const [rating, setRating] = useState(data.rating);
+
   return (
     <View style={styles.container}>
       {/* logo */}
       <Image style={styles.logo} source={require("./logo.png")} />
       <Text>{"\n"}</Text>
       {/* song entry details */}
-      <Text style={styles.username}>username</Text>
-      <Text style={styles.song}>SONG</Text>
-      <Text style={styles.artist}>by Artist</Text>
-      <Rating readonly={true} tintColor="#17222c" />
+      <Text style={styles.username}>{username}</Text>
+      <Text style={styles.song}>{song}</Text>
+      <Text style={styles.artist}>by {artist}</Text>
+      <Rating readonly={true} tintColor="#17222c" startingValue={rating} />
       <Text>{"\n"}</Text>
+      {/* add condition to only show if username is same as user who is logged in */}
       <View style={styles.iconContainer}>
         {/* edit button */}
         <FontAwesomeIcon
