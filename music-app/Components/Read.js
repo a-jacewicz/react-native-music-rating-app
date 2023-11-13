916ac9282e2ac42463d18440bd7ff9ed7e9ca7d4
khentrekin <khentrekin5578@gmail.com>
2023-11-13 10:38:14 -0500

    Created useEffect and map for list entries

--- music-app/Components/Read.js
+++ music-app/Components/Read.js
@@ -1,30 +1,75 @@
-import { StyleSheet, Text, View, Image, Button } from "react-native";
-import React from "react";
-import logo from "./logo.png";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 
 export default function Read() {
+  const [songData, setSongData] = useState([]);
+
+  //   gets list of song entries
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://172.21.250.15/index.php/rating/view"
+        );
+        const data = await response.json();
+        setSongData(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      {/* logo */}
-      <Image style={styles.logo} source={require("./logo.png")} />
-      <Text>{"\n"}</Text>
-      <Text>HEYYYY!</Text>
-      {/* list -- scrolling feature */}
-      <Text>{"\n"}</Text>
-      {/* create button */}
-      <Button
-        title="Create"
-        color={"#FF1CC0"}
-        //   onPress={handleCreate}
-      ></Button>
-      <Text>{"\n"}</Text>
-      {/* logout button */}
-      <Button
-        title="Logout"
-        color={"#6c80ff"}
-        //   onPress={handleLogout}
-      ></Button>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {/* logo */}
+        <Image style={styles.logo} source={require("./logo.png")} />
+        <Text>{"\n"}</Text>
+        {/* list -- scrolling feature */}
+        {/* visual test -- to be deleted */}
+        <Text style={styles.entry}>
+          <Text style={styles.song}>SONG </Text>
+          <Text style={styles.artist}> by Artist</Text>
+          <Text style={styles.username}> | username</Text>
+        </Text>
+        {/* ... */}
+        <View>
+          {songData &&
+            songData.map((item) => (
+              <View key={item.id}>
+                <Text style={styles.entry}>
+                  <Text style={styles.song}>{item.song} </Text>
+                  <Text style={styles.artist}> {item.artist}</Text>
+                  <Text style={styles.username}> | {item.username}</Text>
+                </Text>
+              </View>
+            ))}
+        </View>
+        <Text>{"\n"}</Text>
+        {/* create button */}
+        <Button
+          title="Create"
+          color={"#FF1CC0"}
+          //   onPress={handleCreate}
+        ></Button>
+        <Text>{"\n"}</Text>
+        {/* logout button */}
+        <Button
+          title="Logout"
+          color={"#6c80ff"}
+          //   onPress={handleLogout}
+        ></Button>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -39,5 +84,24 @@ const styles = StyleSheet.create({
   logo: {
     width: 160,
     height: 82,
+  },
+  entry: {
+    color: "white",
+    backgroundColor: "#4A0074",
+    paddingTop: 16,
+    paddingBottom: 16,
+    padding: 16,
+    marginBottom: 10,
+  },
+  song: {
+    // alignItems: "center",
+    fontWeight: "bold",
+    color: "#e1d4f3",
+  },
+  artist: {
+    color: "#ccbed8",
+  },
+  username: {
+    color: "#161616",
   },
 });
