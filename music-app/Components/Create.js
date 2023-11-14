516bfafb1394835f7615c9b17824b5a38e1ebe72
khentrekin <khentrekin5578@gmail.com>
2023-11-13 21:46:46 -0500

    Adding styling

--- music-app/Components/Create.js
+++ music-app/Components/Create.js
@@ -6,6 +6,7 @@ import {
   Button,
   TouchableOpacity,
   StyleSheet,
+  Image,
 } from "react-native";
 // import AsyncStorage from "@react-native-async-storage/async-storage";
 import StarRating from "react-native-star-rating";
@@ -61,50 +62,97 @@ export default function Create() {
   };
 
   return (
-    <View>
-      <Text>Create Rating</Text>
-      <View>
-        <Text>Artist</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Artist"
-          autoCompleteType="off"
-          autoCapitalize="none"
-          keyboardType="default"
-          value={artist}
-          onChangeText={(text) => setArtist(text)}
-          required
-        />
-        <Text>Song</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Title"
-          autoCompleteType="off"
-          value={song}
-          onChangeText={(text) => setSong(text)}
-          required
-        />
-        <Text>Rating</Text>
-        <StarRating
-          disabled={false}
-          maxStars={5}
-          rating={rating}
-          selectedStar={(rating) => setRating(rating)}
-          fullStarColor={"gold"}
-          emptyStarColor={"grey"}
-          starSize={30}
-        />
-        <Button title="Rate" onPress={handleCreate}></Button>
+    <View style={styles.app}>
+      {/* logo */}
+      <Image style={styles.logo} source={require("./logo.png")} />
+      <Text>{"\n"}</Text>
+      <View style={styles.container}>
+        {/* logo
+        <Image style={styles.logo} source={require("./logo.png")} />
+        <Text>{"\n"}</Text> */}
+        <Text style={styles.title}>Create Rating</Text>
+        <View style={styles.form}>
+          <Text style={styles.label}>Artist</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Artist"
+            autoCompleteType="off"
+            autoCapitalize="none"
+            keyboardType="default"
+            value={artist}
+            onChangeText={(text) => setArtist(text)}
+            required
+          />
+          <Text style={styles.label}>Song</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Title"
+            autoCompleteType="off"
+            value={song}
+            onChangeText={(text) => setSong(text)}
+            required
+          />
+          <Text style={styles.label}>Rating</Text>
+          <StarRating
+            disabled={false}
+            maxStars={5}
+            rating={rating}
+            selectedStar={(rating) => setRating(rating)}
+            fullStarColor={"#FF1CC0"}
+            emptyStarColor={"black"}
+            starSize={30}
+          />
+          <Text>{"\n"}</Text>
+          <Button
+            disabled={!artist || !song || !rating}
+            title="Rate"
+            onPress={handleCreate}
+          ></Button>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: "#17222c",
+    alignItems: "center",
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#6c80ff",
+    width: "80%",
+    maxWidth: 420,
+    minHeight: 400,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    padding: 16,
+  },
+  logo: {
+    width: 160,
+    height: 82,
+    marginTop: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  form: {
+    width: "80%",
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
   input: {
     height: 40,
-    borderColor: "grey",
-    backgroundColor: "white",
+    borderColor: "black",
+    backgroundColor: "#dce0fc",
     borderWidth: 1,
     marginBottom: 16,
     paddingHorizontal: 10,
