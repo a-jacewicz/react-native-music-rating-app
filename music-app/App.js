c0b79c0d71f540aa140478367e311a50b6f50eef
khentrekin <khentrekin5578@gmail.com>
2023-11-15 00:51:15 -0500

    minor edits

--- music-app/App.js
+++ music-app/App.js
@@ -25,11 +25,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#17222c",
-//     alignItems: "center",
-//   },
-// });
